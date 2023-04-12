59eed442b4f40ec46754f520255dc92acc71cd21
roberto28gm <roberto28gm@gmail.com>
2023-04-12 13:25:58 +0200

    Added angular material. Created project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './game-tracking/pages/main/main.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/game-tracking/interfaces/game.interface.ts
@@ -0,0 +1,8 @@
+
+export interface Game {
+    name: string;
+    completed: boolean;
+    hours?: number;
+    dateCompleted?: Date;
+    image?: string;
+}
