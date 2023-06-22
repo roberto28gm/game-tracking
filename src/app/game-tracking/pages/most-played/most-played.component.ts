ac795105c5c23d61eb33f6b89273921a00151ea3
@roberto28gm <roberto28gm@gmail.com>
2023-06-22 10:54:05 +0200

    "Pending Games" and "Most Played Games" pages created

--- src/app/game-tracking/pages/most-played/most-played.component.ts
+++ src/app/game-tracking/pages/most-played/most-played.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../interfaces/game.interface';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-most-played',
   templateUrl: './most-played.component.html'
 })
-export class MostPlayedComponent {
+export class MostPlayedComponent implements OnInit {
+  tableData: Game[] = [];
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(): void {
+    const data = this.dataService.readData();
+    this.tableData = data.sort((a, b) => {
+      if (a.hours && b.hours) {
+        return b.hours - a.hours    
+      } else {
+        return 0;
+      }
+      
+    });
+
+  }
 
 }
--- src/app/game-tracking/pages/pending/pending.component.ts
+++ src/app/game-tracking/pages/pending/pending.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../interfaces/game.interface';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-pending',
   templateUrl: './pending.component.html'
 })
-export class PendingComponent {
+export class PendingComponent implements OnInit {
+  tableData: Game[] = [];
 
+  constructor(private dataService: DataService){}
+
+  ngOnInit(): void {
+    const data = this.dataService.readData();
+    this.tableData = data.filter(x => x.completed === false);
+
+  }
 }
